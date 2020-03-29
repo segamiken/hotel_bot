55509341fc7147e4eed6183755677421563c7482
KEN <ken.s.vollyball@gmail.com>
2020-03-29 17:22:24 +0900

    ユーザー名取得、修正。

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
 
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
-        const profile =  await client.getProfile(event.source.userId);
+        const profile = client.getProfile(event.source.userId);
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
             // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
